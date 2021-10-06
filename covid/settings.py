c4db263b8a974320ec59451369d41f21a0deaba0
Divyansh Dwivedi <d_divyansh@yahoo.com>
2021-10-06 19:48:44 +0530

    Changes in settings.py

--- covid/settings.py
+++ covid/settings.py
@@ -136,6 +136,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = '/login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
